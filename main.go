f0ca49f3e54b4d18ae5d57cd941ea0e85890bee6
Avinash Kumar Maurya <140153168+dk-avinash@users.noreply.github.com>
2025-12-29 10:51:55 +0100

    Project Foundation - 2 Fix workflow

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-    "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-    "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 // version is set during build via ldflags
 var version = "0.1.0-alpha.1"
 
 func main() {
-    plugin.Serve(&plugin.ServeOpts{
-        ProviderFunc: func() *schema.Provider {
-            return &schema.Provider{
-                Schema:         map[string]*schema.Schema{},
-                ResourcesMap:   map[string]*schema.Resource{},
-                DataSourcesMap: map[string]*schema.Resource{},
-            }
-        },
-    })
+	// Handle --version flag
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Println("pennypecker version", version)
+		return
+	}
+
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return &schema.Provider{
+				Schema:         map[string]*schema.Schema{},
+				ResourcesMap:   map[string]*schema.Resource{},
+				DataSourcesMap: map[string]*schema.Resource{},
+			}
+		},
+	})
 }
